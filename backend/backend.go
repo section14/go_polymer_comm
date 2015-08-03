ebbbda3240157e06e068464697b28ee246b9d3b6
section14 <henduism@gmail.com>
2015-08-02 22:32:10 -0400

    loads of changes

--- backend/backend.go
+++ backend/backend.go
@@ -1,23 +1,74 @@
 package backend
 
 import (
+	//standard library
 	"fmt"
 	"net/http"
-    //"log"
-    //"appengine"
+	"log"
+	//"appengine"
+
+	//third party
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	//"github.com/gorilla/securecookie"
+
+	//my imports
+	"github.com/section14/go_polymer_comm_pkg/model"
 )
 
+//setup session
+var store = sessions.NewCookieStore([]byte("auto-intro-amigo-status"))
+
 func init() {
-    http.HandleFunc("/api/mogo/", handlerMogo)
-	http.HandleFunc("/api/", handler)
+	r := mux.NewRouter();
+
+	r.HandleFunc("/api/", handler)
+	r.HandleFunc("/api/test/", testHandler)
+	r.HandleFunc("/api/bro/", broHandler)
+	http.Handle("/", r)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	//c := appengine.NewContext(r)
-    //log.Println("Move maker supreme")
-    fmt.Fprint(w, "Hekkraaae")
+	session, _ := store.Get(r, "app-session")
+	session.Values["rude boy"] = "on the scene run bwoy select"
+	session.Save(r, w)
+
+    fmt.Fprint(w, "Root level, nothing here")
 }
 
-func handlerMogo(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Mogo crew")
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "app-session")
+	rudeBoy := session.Values["rude boy"]
+
+	userModel := model.User {
+		Email: "test2",
+		Password: "test2",
+		Role: 1,
+	}
+
+	err := userModel.AddUser(w,r)
+
+	if err != nil {
+		//hanle error
+		log.Println(err)
+	}
+
+	log.Println(rudeBoy)
+}
+
+func broHandler(w http.ResponseWriter, r *http.Request) {
+	userModel := model.User{}
+
+	res, err := userModel.GetAllUsers(w,r)
+
+	if err != nil {
+		//hanle error
+	}
+
+	fmt.Fprint(w, res)
+}
+
+func adminHander(w http.ResponseWriter, r *http.Request) {
+	//need to make sure user is logged in before they can get here
+	//just return a 500, omit error
 }
